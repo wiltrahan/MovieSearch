169e7d8273468e00a0f8b61051925ab275cf75b1
Wil <william.trahan@virginpulse.com>
2020-04-05 16:20:37 -0400

    Initial Commit

---
+++ MovieSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieSearch
+//
+//  Created by William Trahan on 4/5/20.
+//  Copyright © 2020 Wil Trahan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
